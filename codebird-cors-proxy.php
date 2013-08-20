3ba28f8867c97f26c0de74a6207781b615deb292
J.M <me@mynetx.net>
2013-08-20 18:15:42 +0200

    1.2.2 version

--- codebird-cors-proxy.php
+++ codebird-cors-proxy.php
@@ -6,7 +6,7 @@ namespace CodeBird;
  * Proxy to the Twitter API, adding CORS headers to replies.
  *
  * @package codebird
- * @version 1.2.1
+ * @version 1.2.2
  * @author J.M. <me@mynetx.net>
  * @copyright 2013 J.M. <me@mynetx.net>
  *
